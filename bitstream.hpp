3d2545996a32fced1b8cdfee71d2312e1849fdf4
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2023-07-04 23:39:39 +0900

    Improve speed for writing bits into codestream buffer

--- bitstream.hpp
+++ bitstream.hpp
@@ -5,13 +5,66 @@
 
 #include "jpgmarkers.hpp"
 
+// #define NAIVE
+
 class bitstream {
  private:
   int32_t bits;
-  uint8_t tmp;
+  uint64_t tmp;
   std::vector<uint8_t> stream;
 
+  inline void emit_qword(uint64_t d) {
+    uint8_t val;
+    if (d & 0x8080808080808080 & ~(d + 0x0101010101010101)) {
+      for (int i = 56; i >= 0; i -= 8) {
+        val = d >> i;
+        put_byte(val);
+        if (val == 0xFF) {
+          put_byte(0x00);
+        }
+      }
+    } else {
+      for (int i = 56; i >= 0; i -= 8) {
+        val = d >> i;
+        put_byte(val);
+      }
+    }
+  }
+
+  [[maybe_unused]] inline void emit_dword(uint32_t d) {
+    uint8_t val;
+    if (d & 0x80808080 & ~(d + 0x01010101)) {
+      for (int i = 24; i >= 0; i -= 8) {
+        val = d >> i;
+        put_byte(val);
+        if (val == 0xFF) {
+          put_byte(0x00);
+        }
+      }
+    } else {
+      for (int i = 24; i >= 0; i -= 8) {
+        val = d >> i;
+        put_byte(val);
+      }
+    }
+  }
+
   void flush() {
+#if not defined(NAIVE)
+    int n = (bits + 8 - 1) / 8;
+    tmp <<= 8 * n - bits;
+    tmp |= ~(0xFFFFFFFFFFFFFFFFUL << (8 * n - bits));
+    uint64_t mask = 0xFF00000000000000UL >> (64 - n * 8);
+    for (int i = n - 1; i >= 0; --i) {
+      uint8_t upper_byte = (tmp & mask) >> (8 * i);
+      put_byte(upper_byte);
+      if (upper_byte == 0xFF) {
+        put_byte(0x00);
+      }
+      bits -= 8;
+      mask >>= 8;
+    }
+#else
     if (bits) {
       // stuff bit = '1'
       uint8_t stuff = 0xFFU >> bits;
@@ -25,20 +78,38 @@ class bitstream {
     }
     tmp  = 0;
     bits = 0;
+#endif
   }
 
  public:
   bitstream() : bits(0), tmp(0) { put_word(SOI); }
 
-  void put_byte(uint8_t d) { stream.push_back(d); }
+  inline void put_byte(uint8_t d) { stream.push_back(d); }
 
-  void put_word(uint16_t d) {
+  inline void put_word(uint16_t d) {
     put_byte(d >> 8);
     put_byte(d & 0xFF);
   }
 
-  void put_bits(uint32_t cwd, uint32_t len) {
+  inline void put_bits(uint32_t cwd, uint32_t len) {
     assert(len != 0);
+#if not defined(NAIVE)
+    if ((bits + len) < 64) {
+      tmp <<= len;
+      tmp |= (uint64_t)cwd & ((1 << len) - 1);
+      bits += len;
+    } else {
+      tmp <<= 64 - bits;
+      uint64_t mask = (~(0xFFFFFFFFU << (bits + len - 64)));
+      uint64_t val  = cwd & mask;
+      cwd &= ((1 << len) - 1);
+      cwd >>= (bits + len - 64);
+      tmp |= cwd;
+      emit_qword(tmp);
+      tmp  = val;
+      bits = bits + len - 64;
+    }
+#else
     while (len) {
       tmp <<= 1;
       tmp |= (cwd >> (len - 1)) & 1;
@@ -54,6 +125,7 @@ class bitstream {
         bits = 0;
       }
     }
+#endif
   }
 
   auto finalize() {
