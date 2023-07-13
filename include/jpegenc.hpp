a6c32d0c62cb072da2b28fe50322627bca5cd0c6
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2023-07-13 13:48:34 +0900

    Make the codestream member private

--- include/jpegenc.hpp
+++ include/jpegenc.hpp
@@ -8,9 +8,9 @@ namespace jpegenc {
 class jpeg_encoder {
  private:
   std::unique_ptr<class jpeg_encoder_impl> impl;
+  std::vector<uint8_t> codestream;
 
  public:
-  std::vector<uint8_t> codestream;
   jpeg_encoder(const std::string &infile, int &QF, int &YCCtype);
   void invoke();
   int32_t get_width();
