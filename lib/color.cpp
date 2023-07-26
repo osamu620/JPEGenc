a622354534c06773a11ffea13b8768038776ec3d
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2023-07-26 10:05:19 +0900

    Fix

--- lib/color.cpp
+++ lib/color.cpp
@@ -94,7 +94,7 @@ HWY_ATTR void subsample_simd(uint8_t *HWY_RESTRICT in, std::vector<int16_t *> ou
   size_t pos        = 0;
   size_t pos_Chroma = 0;
   HWY_CAPPED(uint8_t, 16) u8;
-  HWY_CAPPED(int16_t, 16) s16;
+  HWY_CAPPED(int16_t, 8) s16;
   auto c128  = Set(u8, 128);
   auto vhalf = Set(s16, half);
   switch (YCCtype) {
