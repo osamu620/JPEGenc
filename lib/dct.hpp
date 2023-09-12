d89483caafc8292ca9e7d99ec5189318dbf8f376
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2023-09-12 16:27:42 +0900

    Delete unnecessary EXPORT

--- lib/dct.hpp
+++ lib/dct.hpp
@@ -4,6 +4,5 @@
 #include <vector>
 
 namespace jpegenc_hwy {
-HWY_ATTR void dct2_core(int16_t *HWY_RESTRICT data);
 void dct2(std::vector<int16_t *> in, int width, int mcu_height, int YCCtype);
 }  // namespace jpegenc_hwy
