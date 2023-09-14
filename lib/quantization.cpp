fa15d5e9ee66231816a79f62793b11570db695fd
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2023-09-14 16:26:04 +0900

    Experimental changes: Using MCU buffer (6)

--- lib/quantization.cpp
+++ lib/quantization.cpp
@@ -14,33 +14,6 @@ namespace jpegenc_hwy {
 namespace HWY_NAMESPACE {
 namespace hn = hwy::HWY_NAMESPACE;
 
-HWY_ATTR void quantize_core(int16_t *HWY_RESTRICT data, const int *HWY_RESTRICT qtable) {
-#if HWY_TARGET != HWY_SCALAR
-  const hn::ScalableTag<int16_t> d16;
-  const hn::ScalableTag<int32_t> d32;
-  const auto half = hn::Set(d32, 1 << 15);
-  for (int i = 0; i < DCTSIZE2; i += Lanes(d16)) {
-    auto ql = Load(d32, qtable + i);
-    auto qh = Load(d32, qtable + i + Lanes(d32));
-    auto v  = Load(d16, data + i);
-    auto vl = PromoteLowerTo(d32, v);
-    auto vh = PromoteUpperTo(d32, v);
-
-    vl = MulAdd(vl, ql, half);
-    vh = MulAdd(vh, qh, half);
-    vl = hn::ShiftRight<16>(vl);
-    vh = hn::ShiftRight<16>(vh);
-    Store(OrderedDemote2To(d16, vl, vh), d16, data + i);
-  }
-#else
-  int shift = 16;
-  int half  = 1 << (shift - 1);
-  for (int i = 0; i < DCTSIZE2; ++i) {
-    data[i] = static_cast<int16_t>((data[i] * qtable[i] + half) >> shift);
-  }
-#endif
-}
-
 }  // namespace HWY_NAMESPACE
 }  // namespace jpegenc_hwy
 
