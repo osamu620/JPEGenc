acad6ea35911a2d7c7a16d465aa807fb5bd788c6
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2023-09-21 16:06:15 +0900

    Change the way to hold the huffman tables

--- lib/block_coding.hpp
+++ lib/block_coding.hpp
@@ -2,15 +2,29 @@
 
 #include "bitstream.hpp"
 
+#include <hwy/aligned_allocator.h>
+
 namespace jpegenc_hwy {
+
 struct huff_info {
-  const uint16_t *HWY_RESTRICT DC_cwd;
-  const uint16_t *HWY_RESTRICT AC_cwd;
-  const uint8_t *HWY_RESTRICT DC_len;
-  const uint8_t *HWY_RESTRICT AC_len;
-  huff_info(const uint16_t *HWY_RESTRICT dc, const uint16_t *HWY_RESTRICT ac,
-            const uint8_t *HWY_RESTRICT dl, const uint8_t *HWY_RESTRICT al)
-      : DC_cwd(dc), AC_cwd(ac), DC_len(dl), AC_len(al) {}
+  std::unique_ptr<uint8_t[], hwy::AlignedFreer> DC_;
+  std::unique_ptr<uint8_t[], hwy::AlignedFreer> AC_;
+  uint16_t *DC_cwd;
+  uint16_t *AC_cwd;
+  uint8_t *DC_len;
+  uint8_t *AC_len;
+  huff_info(const uint16_t *dc, const uint16_t *ac, const uint8_t *dl, const uint8_t *al) {
+    DC_    = hwy::AllocateAligned<uint8_t>(static_cast<size_t>(16 + 32));
+    AC_    = hwy::AllocateAligned<uint8_t>(static_cast<size_t>(256 + 512));
+    DC_cwd = (uint16_t *)DC_.get();
+    AC_cwd = (uint16_t *)AC_.get();
+    DC_len = DC_.get() + 32;
+    AC_len = AC_.get() + 512;
+    memcpy(DC_cwd, dc, sizeof(uint16_t) * 16);
+    memcpy(AC_cwd, ac, sizeof(uint16_t) * 256);
+    memcpy(DC_len, dl, 16);
+    memcpy(AC_len, al, 256);
+  }
 };
 
 void encode_lines(std::vector<int16_t *> &in, int16_t *HWY_RESTRICT mcu, int width, int mcu_height,
